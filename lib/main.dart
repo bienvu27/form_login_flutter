3f2875cca5f984c2b33771ca65f8c9084e195d8f
bienvx1999@gmail.com <bienvx1999@gmail.com>
2023-02-09 16:03:03 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -15,16 +15,85 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         body: Container(
+          padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
           constraints: const BoxConstraints.expand(),
           color: Colors.white,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
-            children: const [
-              FlutterLogo(),
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: Container(
+                    width: 70,
+                    height: 70,
+                    padding: const EdgeInsets.all(15),
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle, color: Color(0xffd8d8d8)),
+                    child: const FlutterLogo()),
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 60),
+                child: Text(
+                  'Hello\nWelcome Back',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: 30),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: TextField(
+                  style: TextStyle(fontSize: 18, color: Colors.black),
+                  decoration: InputDecoration(
+                      labelText: 'UserName',
+                      labelStyle:
+                          TextStyle(color: Color(0xff888888), fontSize: 15)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: Stack(
+                  alignment: AlignmentDirectional.centerEnd,
+                  children: const [
+                    TextField(
+                      style: TextStyle(fontSize: 18, color: Colors.black),
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          labelText: 'PassWord',
+                          labelStyle: TextStyle(
+                              color: Color(0xff888888), fontSize: 15)),
+                    ),
+                    Text(
+                      'SHOW',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 13,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                  width: double.infinity,
+                  height: 56,
+                  child: ElevatedButton(
+                      onPressed: onSignInClicked,
+                      style: ElevatedButton.styleFrom(
+                          side: const BorderSide(
+                              width: 1,
+                            color: Colors.blue
+                          )
+                      ),
+                      child: const Text(
+                        'SIGN IN',
+                      )))
             ],
           ),
         ),
       ),
     );
   }
+  void onSignInClicked(){}
 }
